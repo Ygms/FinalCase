6cbfdd898d93b2eedefcc0e5e8eae27437b8a977
Yağmur <86566611+Ygms@users.noreply.github.com>
2025-09-10 15:03:08 +0300

    added floating animation

--- su_kahramani/lib/home_page.dart
+++ su_kahramani/lib/home_page.dart
@@ -10,129 +10,178 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(
+      begin: -10,
+      end: 10,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
       backgroundColor: Color.fromARGB(255, 224, 244, 255),
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 138, 205, 215),
-        title: Center(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 5.0),
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                radius: 3.0,
-                colors: [
-                  Color.fromARGB(255, 0, 162, 206),
-                  Color.fromARGB(255, 138, 205, 215),
-                ],
-              ),
-              border: Border.all(color: Colors.white, width: 2),
-              borderRadius: BorderRadius.circular(25.0),
-            ),
-            child: Text(
-              "Su Kahramanı",
-              style: TextStyle(
-                fontFamily: "Grandstander",
-                fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
+      appBar: _appBar(),
+
+      body: Center(
+        child: Column(
+          children: [_speechText(), _mainCharImg(_animation), _startButton()],
+        ),
+      ),
+
+      bottomNavigationBar: _navigationBar(context),
+    );
+  }
+
+  Center _mainCharImg(Animation<double> animation) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return Transform.translate(
+            offset: Offset(0, animation.value),
+            child: child,
+          );
+        },
+        child: Container(
+          height: 400,
+          width: 450,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/character/main_char.png"),
+              fit: BoxFit.fitHeight,
             ),
           ),
         ),
       ),
+    );
+  }
 
-      body: Center(
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 30),
-                padding: EdgeInsets.all(10.0),
-                height: 100,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color.fromARGB(255, 50, 50, 89),
-                ),
-                child: TypeWriter.text(
-                  "Merhaba, <kullanıcı adı>! \n Su Kahramanı olmak ister misin?",
-                  maintainSize: true,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 20.0, fontFamily: "Grandstander", color: Colors.white),
-                  duration: const Duration(milliseconds: 50),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                height: 400,
-                width: 450,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/character/main_char.png"),
-                    fit: BoxFit.fitHeight,
-                  ),
-                ),
-              ),
-            ),
+  Center _startButton() {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        height: 50,
+        width: 180,
+        decoration: BoxDecoration(
+          border: Border.all(color: Color.fromARGB(255, 50, 50, 89), width: 2),
+          borderRadius: BorderRadius.circular(10.0),
 
-            Center(
-              child: Container(
-                padding: EdgeInsets.all(10.0),
-                height: 50,
-                width: 180,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Color.fromARGB(255, 50, 50, 89),
-                    width: 2,
-                  ),
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color.fromARGB(255, 50, 50, 89),
-
-                  gradient: RadialGradient(
-                    radius: 3.0,
-                    colors: [
-                      Color.fromARGB(255, 190, 241, 255),
-                      Color.fromARGB(255, 0, 162, 206),
-                    ],
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    "Hikayeye Başla",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 50, 50, 89),
-                      fontFamily: "Grandstander",
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
+          //color: Color.fromARGB(255, 50, 50, 89),
+          gradient: RadialGradient(
+            radius: 3.0,
+            colors: [
+              Color.fromARGB(255, 190, 241, 255),
+              Color.fromARGB(255, 0, 162, 206),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Text(
+            "Hikayeye Başla",
+            style: TextStyle(
+              color: Color.fromARGB(255, 50, 50, 89),
+              fontFamily: "Grandstander",
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
-          ],
+          ),
         ),
       ),
+    );
+  }
 
-      bottomNavigationBar: Theme(
-        data: Theme.of(
-          context,
-        ).copyWith(iconTheme: IconThemeData(color: Colors.white)),
-        child: CurvedNavigationBar(
-          animationCurve: Easing.standardDecelerate,
-          buttonBackgroundColor: Color.fromARGB(255, 0, 162, 206),
-          animationDuration: Duration(milliseconds: 400),
-          backgroundColor: Color.fromARGB(255, 224, 244, 255),
-          color: Color.fromARGB(255, 138, 205, 215),
-          items: [
-            Icon(Icons.check_box, size: 30),
-            SvgPicture.asset('assets/drop.svg', width: 35, height: 35),
-            Icon(Icons.account_circle, size: 30),
-          ],
+  Center _speechText() {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.all(10.0),
+        height: 100,
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Color.fromARGB(255, 50, 50, 89),
+        ),
+        child: TypeWriter.text(
+          "Merhaba, <kullanıcı adı>! \n Su Kahramanı olmak ister misin?",
+          maintainSize: true,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 20.0,
+            fontFamily: "Grandstander",
+            color: Colors.white,
+          ),
+          duration: const Duration(milliseconds: 50),
+        ),
+      ),
+    );
+  }
+
+  Theme _navigationBar(BuildContext context) {
+    return Theme(
+      data: Theme.of(
+        context,
+      ).copyWith(iconTheme: IconThemeData(color: Colors.white)),
+      child: CurvedNavigationBar(
+        animationCurve: Easing.standardDecelerate,
+        buttonBackgroundColor: Color.fromARGB(255, 0, 162, 206),
+        animationDuration: Duration(milliseconds: 400),
+        backgroundColor: Color.fromARGB(255, 224, 244, 255),
+        color: Color.fromARGB(255, 138, 205, 215),
+        items: [
+          Icon(Icons.check_box, size: 30),
+          SvgPicture.asset('assets/drop.svg', width: 35, height: 35),
+          Icon(Icons.account_circle, size: 30),
+        ],
+      ),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: Color.fromARGB(255, 138, 205, 215),
+      title: Center(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4.0),
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              radius: 3.0,
+              colors: [
+                Color.fromARGB(255, 0, 162, 206),
+                Color.fromARGB(255, 138, 205, 215),
+              ],
+            ),
+            border: Border.all(color: Colors.white, width: 2),
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          child: Text(
+            "Su Kahramanı",
+            style: TextStyle(
+              fontFamily: "Grandstander",
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
